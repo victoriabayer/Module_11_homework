ea987909cd545ffc478cce571d9a72c7b1c585a4
Victoria Bayer <victoriabayer733@gmail.com>
2024-06-19 13:24:23 +0300

    Module 11

---
+++ Module_11.4/module_11.4.js.js
@@ -0,0 +1,6 @@
+const width = document.documentElement.clientWidth;
+const height = document.documentElement.clientHeight;
+
+function showScreenSize() {
+  alert("Ширина экрана: " + width + "px, Высота экрана: " + height + "px");
+}
